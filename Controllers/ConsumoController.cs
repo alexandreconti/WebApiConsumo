37057213134f277bb7446494610e377bbf7c5357
Alexandre De Conti <alexandreconti@gmail.com>
2020-10-31 12:12:15 -0300

    Add project files.

---
+++ Controllers/ConsumoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiConsumo.Models;
+
+namespace WebApiConsumo.Controllers
+{
+    public class ConsumoController : ApiController
+    {
+        private Consumo consumo = new Consumo();
+
+        public Consumo Get()
+        {
+            consumo.Ditancia = consumo.Tempo * consumo.VelMedia;
+            consumo.QtdLitros = consumo.Ditancia / consumo.KmLitro;
+            return consumo;
+        }
+    }
+}
---
+++ Models/Consumo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiConsumo.Models
+{
+    public class Consumo
+    {
+        public double VelMedia { get; set; }
+        public double Tempo { get; set; }
+        public double KmLitro { get; set; }
+
+        public double Ditancia { get; set; }
+        public double QtdLitros { get; set; }
+
+        public Consumo()
+        {
+            VelMedia = 10;
+            Tempo = 2.8;
+            KmLitro = 3;
+            Ditancia = 0;
+            QtdLitros = 0;
+        }
+    }
+}
